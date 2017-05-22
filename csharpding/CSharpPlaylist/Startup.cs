a18fcc915cd9c6862a487d441069a2241bc2a8bf
Bram Klapwijk <bram.klapwijk00@outlook.com>
2017-05-22 08:57:42 +0200

    Start
    0.0.1

---
+++ csharpding/CSharpPlaylist/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSharpPlaylist.Startup))]
+namespace CSharpPlaylist
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
